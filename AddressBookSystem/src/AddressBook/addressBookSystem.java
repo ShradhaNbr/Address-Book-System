a9f029575586e01a45e6302d33891b374e28a99a
ShradhaNbr <sradha.nambiar@gmail.com>
2021-07-12 11:22:56 +0530

    Added : contact details into the address book

--- AddressBookSystem/src/AddressBook/addressBookSystem.java
+++ AddressBookSystem/src/AddressBook/addressBookSystem.java
@@ -1,7 +1,9 @@
 package AddressBook;
 
 public class addressBookSystem {
+
 	public static void main(String[] args) {
 		System.out.println("Welcome to Address Book");
+
 	}
 }
---
+++ AddressBookSystem/src/AddressBook/contactDetails.java
@@ -0,0 +1,70 @@
+package AddressBook;
+
+public class contactDetails {
+	String firstName, lastName, address, city, state, email;
+	long zipCode, phoneNumber;
+
+	public String getfirstName() {
+		return firstName;
+	}
+
+	public void setfirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getlastName() {
+		return lastName;
+	}
+
+	public void setlastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getaddress() {
+		return address;
+	}
+
+	public void setaddress(String address) {
+		this.address = address;
+	}
+
+	public String getcity() {
+		return city;
+	}
+
+	public void setcity(String city) {
+		this.city = city;
+	}
+
+	public String getstate() {
+		return state;
+	}
+
+	public void setstate(String state) {
+		this.state = state;
+	}
+
+	public String getemail() {
+		return email;
+	}
+
+	public void setemail(String email) {
+		this.email = email;
+	}
+
+	public long getzipCode() {
+		return zipCode;
+	}
+
+	public void setzipCode(long zipCode) {
+		this.zipCode = zipCode;
+	}
+
+	public long getphoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setphoneNumber(long phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+}
